2b9845dc5ac47cc17bfed94ce5177d786dd32d35
Alan Nguyen <tuan8101@gmail.com>
2021-01-15 21:08:55 -0700

    Add event listener to add user

--- script.js
+++ script.js
@@ -51,5 +51,8 @@ function updateDOM(providedData = data) {
 
 // Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
 function formatMoney(number) {
-  return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
+
+// Event listeners
+addUserBtn.addEventListener('click', getRandomUser);
