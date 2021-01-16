ab757112b2f2dd5c329cddc70f255629983cff9f
Alan Nguyen <tuan8101@gmail.com>
2021-01-15 20:53:21 -0700

    Create getRandomUser function

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const doubleBtn = document.getElementById('double');
 const showMillionairesBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Fetch random user and add money
+async function getRandomUser() {
+  const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+  const targetURL = 'https://randomuser.me/api';
+  const res = await fetch(proxyURL + targetURL);
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+  addDataUser(newUser);
+}
