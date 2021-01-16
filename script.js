fb5fc38d661574c8127662981005ea6cc8e4916a
Alan Nguyen <tuan8101@gmail.com>
2021-01-15 21:06:04 -0700

    Create formatMoney function

--- script.js
+++ script.js
@@ -46,3 +46,8 @@ function updateDOM(providedData = data) {
     main.appendChild(element);
   });
 }
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
